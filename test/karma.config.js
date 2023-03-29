9104cf83cf487603894c91245eeee6d680e39470
RustyNova <rusty.nova.jsb@gmail.com>
2023-03-29 14:58:18 +0200

    Test Update to yarn

--- test/karma.config.js
+++ test/karma.config.js
@@ -46,7 +46,7 @@ module.exports = function(config){
 		port: 9876,
 		colors: true,
 		autoWatch: false,
-		browsers: ['ChromeHeadless'],
+		//browsers: ['ChromeHeadless'],
 		singleRun: true
 	});
 };
